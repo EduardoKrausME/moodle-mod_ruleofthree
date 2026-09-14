d8f1be2f0860c694531c934a051d7947948600a9
Eduardo Kraus <kraus@eduardokraus.com>
2026-09-14 14:40:55 -0300

    V 1.1.0

--- version.php
+++ version.php
@@ -23,9 +23,8 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-
-$plugin->version = 2026091200;
-$plugin->release = "1.0.0";
+$plugin->version = 2026091400;
+$plugin->release = '1.1.0';
 $plugin->component = "mod_ruleofthree";
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
